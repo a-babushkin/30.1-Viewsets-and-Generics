b026aa8ea86a83a93ed67abde889976ad87a20a2
a-babushkin <a.babushkin42@gmail.com>
2025-08-01 23:11:22 +0300

    Materials block

---
+++ materials/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+
+
+class Course(models.Model):
+    """Описание модели Курса"""
+
+    title = models.CharField(
+        max_length=250, verbose_name="наименование", help_text="Введите название курса"
+    )
+    description = models.TextField(
+        verbose_name="описание",
+        blank=True,
+        null=True,
+        help_text="Введите описание курса",
+    )
+    image = models.ImageField(
+        verbose_name="изображение",
+        blank=True,
+        null=True,
+        help_text="Загрузите изображение курса",
+        upload_to="uploads/",
+    )
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = ["title"]
+
+
+class Lesson(models.Model):
+    """Описание модели Урока"""
+
+    title = models.CharField(
+        max_length=250, verbose_name="наименование", help_text="Введите название урока"
+    )
+    description = models.TextField(
+        verbose_name="описание",
+        blank=True,
+        null=True,
+        help_text="Введите описание курса",
+    )
+    image = models.ImageField(
+        verbose_name="изображение",
+        blank=True,
+        null=True,
+        help_text="Загрузите изображение курса",
+        upload_to="uploads/",
+    )
+    video = models.CharField(
+        verbose_name="ссылка на видео",
+        blank=True,
+        null=True,
+        help_text="Введите ссылку на видео курса",
+    )
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="course", verbose_name="курс"
+    )
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+        ordering = ["title"]
---
+++ materials/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from materials.apps import MaterialsConfig
+from materials.views import (CourseViewSet, LessonCreateApiView,
+                             LessonDestroyApiView, LessonListApiView,
+                             LessonRetrieveApiView, LessonUpdateApiView)
+
+app_name = MaterialsConfig.name
+
+router = SimpleRouter()
+router.register(r"course", CourseViewSet, basename="course")
+# router.register(r'lesson', LessonViewSet, basename='lesson')
+
+
+urlpatterns = [
+    path("lesson/", LessonListApiView.as_view(), name="lesson_list"),
+    path("lesson/<int:pk>/", LessonRetrieveApiView.as_view(), name="lesson_retrieve"),
+    path("lesson/create/", LessonCreateApiView.as_view(), name="lesson_create"),
+    path(
+        "lesson/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lesson_delete"
+    ),
+    path(
+        "lesson/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lesson_update"
+    ),
+]
+urlpatterns += router.urls
