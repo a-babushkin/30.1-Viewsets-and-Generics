e673a1a997085473cebc5cb0028022811a10653a
a-babushkin <a.babushkin42@gmail.com>
2025-08-29 21:03:00 +0300

    Tasks 1,2,3

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "users",
     "materials",
     "drf_yasg",
+    'django_celery_beat'
 ]
 
 REST_FRAMEWORK = {
@@ -121,3 +122,29 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "users.User"
+
+# Настройки Celery
+CELERY_BROKER_URL = os.getenv("REDIS_LOCATION")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_LOCATION")
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+CELERY_BEAT_SCHEDULE = {
+    "check_last_login": {
+        "task": "materials.tasks.check_last_login",
+        "schedule": timedelta(seconds=20),
+    }
+}
+
+# Настройки почты
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS").lower() in ["true", "yes"]
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL").lower() in ["true", "yes"]
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
---
+++ materials/services.py
@@ -0,0 +1,54 @@
+import logging
+import os
+from datetime import timedelta
+
+from django.core.mail import send_mail
+from django.utils import timezone
+
+from config import settings
+from materials.models import Subscription, Course
+from users.models import User
+
+logger = logging.getLogger(__name__)
+log_file_path = os.path.join(settings.BASE_DIR, "logs", "mailing_send.log")
+file_handler = logging.FileHandler(log_file_path, encoding="utf-8")
+formatter = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+
+def send_update_course(pk):
+    """Отправка рассылки по требованию"""
+    course_subscriptions = Subscription.objects.filter(course=pk)
+    course_title = Course.objects.filter(id=pk).first().title
+
+    from_email = settings.EMAIL_HOST_USER
+    subject = "Обновление курса"
+    message = f'Курс {course_title} обновлен.'
+
+    for subscription in course_subscriptions:
+        if not subscription.user.email:
+            logger.warning(f"У получателя {subscription} отсутствует действительный адрес электронной почты.")
+            break
+        try:
+            send_mail(subject, message, from_email, [subscription.user.email])
+            logger.info(
+                f"На почту: {subscription.user.email} отправлено письмо про обновление курса: {course_title}."
+            )
+
+        except Exception as e:
+            logger.error(f"Ошибка отправки: {e}")
+
+
+def check_last_login_and_block():
+    """Проверка последнего входа пользователей и отключение неактивных пользователей"""
+    users = User.objects.exclude(last_login__isnull=True)
+    today = timezone.now().today()
+    for user in users:
+        if today - user.last_login > timedelta(days=30):
+            user.is_active = False
+            user.save()
+            print(f'Пользователь {user.email} не входил больше месяца и отключен')
+        else:
+            print("OK!")
